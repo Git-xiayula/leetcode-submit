dd3f577bfa8ba6298ee9670ff330f1460e0b73a4
xiayula <xiayula2014@gmail.com>
2016-07-12 09:58:28 +0800

    2 sum

--- 2sum/2sum.cpp
+++ 2sum/2sum.cpp
@@ -8,7 +8,7 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int, int> dict;
         for (int i = 0; i < nums.size(); ++i) {
-            if (dict.count(target - nums[i])) {
+            if (dict.find(target - nums[i]) != dict.end()) {
                 return {dict[target - nums[i]], i};
             }
             dict[nums[i]] = i;
