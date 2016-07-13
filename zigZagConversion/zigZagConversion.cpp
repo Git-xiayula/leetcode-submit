cfa3265f302e3cfe0b56516c9f79180a75549219
xiayula <xiayula2014@gmail.com>
2016-07-13 16:09:05 +0800

    solved zigZagConversion

---
+++ zigZagConversion/zigZagConversion.cpp
@@ -0,0 +1,23 @@
+//
+// Created by xiayula on 16/7/13.
+//
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if(numRows <= 1 || s.size() <= 1) return s;
+        int circle = 2 * numRows - 2;
+        string res = "";
+        for(int i = 0; i < numRows; i++){
+            for(int j = i; j < s.size(); j+=lag){
+                res += s[j];
+                if(i > 0 && i < numRows-1){
+                    int t = j + lag - 2*i;
+                    if(t < s.size()){
+                        res += s[t];
+                    }
+                }
+            }
+        }
+        return res;
+    }
+};
